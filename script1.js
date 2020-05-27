0be17e085d9efb61d26fcab9f5032d8987ffa971
AlenaMikalaeynaLisava <63248586+AlenaMikalaeynaLisava@users.noreply.github.com>
2020-05-27 19:09:08 +0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,73 @@
+
+const calcBtnsEl = document.getElementById("calc-buttons");
+let activeNum = document.getElementById("num1");
+
+let memory;
+calcBtnsEl.addEventListener('click',(event)=>{
+  if(event.target.nodeName ==="BUTTON" && !event.target.dataset.sign &&!event.target.dataset.memory &&!event.target.dataset.register){
+    const attrVal = event.target.getAttribute("data-num");
+    activeNum.innerText += attrVal;
+  }
+  if(event.target.nodeName ==="BUTTON" && event.target.dataset.sign === '+/-'){
+    activeNum.innerText = -activeNum.innerText;
+  }
+ if(event.target.nodeName ==="BUTTON" && (event.target.dataset.sign ==="+"||event.target.dataset.sign ==="-")||event.target.dataset.sign ==="*"||event.target.dataset.sign ==="/"){
+   activeNum=document.getElementById("num2");
+   document.getElementById("sign").innerText=event.target.dataset.sign;
+ }
+
+  if(event.target.nodeName ==="BUTTON" && event.target.dataset.sign === "="){
+    let num1Val = +document.getElementById("num1").innerText;
+    let num2Val = +document.getElementById("num2").innerText;
+    let result;
+    if(document.getElementById("sign").innerText ==="+"){
+    result = num1Val + num2Val;
+    } else if (document.getElementById("sign").innerText ==="-"){
+      result = num1Val - num2Val; 
+    } else if (document.getElementById("sign").innerText === "*"){
+      result = num1Val * num2Val;
+     } else if (document.getElementById("sign").innerText === "/"){
+     result = num1Val / num2Val;
+    } else {result = num1Val;}
+    document.querySelector(" #result span").innerText = result;
+  } 
+  if(event.target.nodeName ==="BUTTON" && event.target.dataset.memory === 'ce'){
+    document.getElementById("num1").innerText="";
+    document.getElementById("num2").innerText="";
+    document.getElementById("sign").innerText="";
+    document.querySelector(" #result span").innerText="";
+    activeNum = document.getElementById("num1");
+  }
+  if(event.target.nodeName ==="BUTTON" && event.target.dataset.memory === "mr"){
+    let num1Val = +document.getElementById("num1").innerText;
+    let num2Val = +document.getElementById("num2").innerText;
+    memory=0;
+    if(document.getElementById("sign").innerText ==="+"){
+      memory = num1Val + num2Val;
+    } else if (document.getElementById("sign").innerText ==="-"){
+      memory = num1Val - num2Val; 
+    } else if (document.getElementById("sign").innerText === "*"){
+      memory = num1Val * num2Val;
+    } else {
+      memory = num1Val / num2Val;}
+      document.getElementById("num1").innerText="";
+      document.getElementById("num2").innerText="";
+      document.getElementById("sign").innerText="";
+      document.querySelector(" #result span").innerText="";
+      activeNum = document.getElementById("num1");
+    //alert(memory);
+  }
+  if(event.target.nodeName ==="BUTTON" && event.target.dataset.memory === 'm+'){
+    activeNum.innerText = memory;
+  }
+
+  if(event.target.nodeName ==="BUTTON" && event.target.dataset.register === '.'){
+    const attrVal = event.target.getAttribute("data-register");
+    activeNum.innerText += attrVal;
+  }
+
+})
+
+
+
+
